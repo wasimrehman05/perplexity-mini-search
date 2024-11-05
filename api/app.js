e34e6797fa2ec3e0da154bca81ee0bc78d00688c
Wasim Rehman <wasim.rehman@anakin.company>
2024-11-05 21:20:24 +0530

    change routes

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
